4ddb5ca5ca4fbb3895c638bb70e5100b2591f3d9
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-09-07 16:18:53 +0200

    Updates

---
+++ precache-manifest.f3e95c28c6eecf25f0f2397e4cf246ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e13bd78c6d3464d581a589e0837039",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "943c77282b52816d067d",
+    "url": "/test/static/css/main.a1c094ad.chunk.css"
+  },
+  {
+    "revision": "5e60f2bdc82f53d8b8a9",
+    "url": "/test/static/js/2.3635f579.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.3635f579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943c77282b52816d067d",
+    "url": "/test/static/js/main.429d472c.chunk.js"
+  },
+  {
+    "revision": "ac72963f3546a51212dc",
+    "url": "/test/static/js/runtime-main.cab68284.js"
+  }
+]);
